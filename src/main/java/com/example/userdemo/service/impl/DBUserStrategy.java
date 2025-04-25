d171ba548bb91109cfffbe0e3c89fc413e016d68
Sarangi Saneesh <sarangisaneesh44@gmail.com>
2025-04-25 19:41:41 +0530

    updated DBUserStrategy and LdapUserStrategy classes.

--- src/main/java/com/example/userdemo/service/impl/DBUserStrategy.java
+++ src/main/java/com/example/userdemo/service/impl/DBUserStrategy.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
-@Service
+
 public class DBUserStrategy implements UserFetchStrategy {
 
     private final UserRepository userRepository;
--- src/main/java/com/example/userdemo/service/impl/LdapUserStrategy.java
+++ src/main/java/com/example/userdemo/service/impl/LdapUserStrategy.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
-@Service
 public class LdapUserStrategy implements UserFetchStrategy {
 
     private final LdapRepository ldapRepository;
